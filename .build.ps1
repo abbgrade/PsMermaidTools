6325af24fcb2237739eb8f5984cf75b096ccb621
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-09-11 10:29:52 +0000

    added ci/cd dependencies

--- .build.ps1
+++ .build.ps1
@@ -3,3 +3,9 @@ $ModuleName = 'PsMermaidTools'
 
 . $PSScriptRoot/tasks/Build.Tasks.ps1
 . $PSScriptRoot/tasks/PsBuild.Tasks.ps1
+
+task InstallBuildDependencies {
+    Install-Module platyPs -Scope CurrentUser
+}
+task InstallTestDependencies {}
+task InstallReleaseDependencies {}
